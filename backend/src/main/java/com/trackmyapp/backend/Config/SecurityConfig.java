80f6a1b3a2059fdce0753699887afc8ad33d3020
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-07-23 11:17:48 +0530

    update configuration for the passwordEncoder

--- backend/src/main/java/com/trackmyapp/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/trackmyapp/backend/Config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.trackmyapp.backend.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -21,5 +23,10 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> httpBasic.disable()); // disable basic auth too
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
