f63350b987de6dec9ae71c3ed1a2416fa5f24da2
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-07-26 11:14:58 +0530

    update the getter and setter using the lombok

--- backend/src/main/java/com/trackmyapp/backend/DTO/LoginResponse.java
+++ backend/src/main/java/com/trackmyapp/backend/DTO/LoginResponse.java
@@ -1,8 +1,10 @@
 package com.trackmyapp.backend.DTO;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LoginResponse {
 
     private String token;
@@ -15,6 +17,7 @@ public class LoginResponse {
         this.token = token;
         this.username = username;
     }
+
 
 
 }
