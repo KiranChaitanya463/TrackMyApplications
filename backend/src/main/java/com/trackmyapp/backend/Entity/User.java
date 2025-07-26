203e2cfee888378a393947544e92e392140f45a0
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-07-26 18:05:28 +0530

    implement the user entity with UserDetails interface which will be used for the authentication in the Spring Security its for role based authentication for now just a deafult user role

--- backend/src/main/java/com/trackmyapp/backend/Entity/User.java
+++ backend/src/main/java/com/trackmyapp/backend/Entity/User.java
@@ -5,8 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -14,7 +20,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,5 +50,15 @@ public class User {
         updateTime=LocalDateTime.now();
     }
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
 }
