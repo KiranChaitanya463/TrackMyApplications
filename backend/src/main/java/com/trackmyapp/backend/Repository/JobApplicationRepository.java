526b6f0b8a7ada74b8d8b111da5fa1f84236378b
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-07-27 11:30:05 +0530

    include the abstract methods FindByUser and FindByIdAndUser in the repository

--- backend/src/main/java/com/trackmyapp/backend/Repository/JobApplicationRepository.java
+++ backend/src/main/java/com/trackmyapp/backend/Repository/JobApplicationRepository.java
@@ -1,11 +1,17 @@
 package com.trackmyapp.backend.Repository;
 
 import com.trackmyapp.backend.Entity.JobApplication;
+import com.trackmyapp.backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication,Long> {
 
     List<JobApplication> findByUserId(Long userId);
+    List<JobApplication> findByUser(User user);
+
+
+    Optional<JobApplication> findByIdAndUser(Long jobId, User user);
 }
