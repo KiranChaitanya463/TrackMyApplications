e039b73e84d2f328d9131fadce66a424e6949573
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-07-28 12:26:41 +0530

    create a JobApplciationResponse DTO for the returning to the frontend rather than a string this DTO dosent contain the User details so it does not confilct with the serialization at backend because user is lazy loaded it conflicts while returning response JobApplication  with json containing user

---
+++ backend/src/main/java/com/trackmyapp/backend/DTO/JobApplicationResponse.java
@@ -0,0 +1,29 @@
+package com.trackmyapp.backend.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class JobApplicationResponse {
+
+    private Long id;
+    private String companyName;
+    private String jobTitle;
+    private String jobLocation;
+    private String jobType;
+    private String applicationLink;
+    private LocalDateTime appliedDate;
+    private String status;
+    private String skills;
+    private String notes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
+
