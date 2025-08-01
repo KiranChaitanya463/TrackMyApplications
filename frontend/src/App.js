1185c9896e3d50e8a0c3f658e230f0aba1a839f8
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-01 18:49:52 +0530

    update App.js to add the dashboard route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element = {<Dashboard />} />
         {/* Signup and Dashboard routes will be added later */}
       </Routes>
     </Router>
