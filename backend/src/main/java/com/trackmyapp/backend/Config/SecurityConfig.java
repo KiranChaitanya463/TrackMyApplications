3f943813b35dcc174e29e8d074f15a2e8aebd9aa
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-04 11:37:30 +0530

    update the cors configuration

--- backend/src/main/java/com/trackmyapp/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/trackmyapp/backend/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.trackmyapp.backend.Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
