6d5a8645836ef3025a97ac3043f0f34c9b3c1fd4
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-04 11:35:14 +0530

    add a new endpoint to get the username to print name on the application

--- backend/src/main/java/com/trackmyapp/backend/Controller/AuthenticationController.java
+++ backend/src/main/java/com/trackmyapp/backend/Controller/AuthenticationController.java
@@ -3,13 +3,12 @@ package com.trackmyapp.backend.Controller;
 import com.trackmyapp.backend.DTO.LoginRequest;
 import com.trackmyapp.backend.DTO.LoginResponse;
 import com.trackmyapp.backend.DTO.UserRegisterRequest;
+import com.trackmyapp.backend.Entity.User;
 import com.trackmyapp.backend.Service.AutenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -28,5 +27,11 @@ public class AuthenticationController {
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
         LoginResponse response=autenticationService.login(request);
         return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<String> getCurrentUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(user.getName()); 
     }
 }
